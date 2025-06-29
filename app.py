d5e2a1b6c58184a838fa6591ffdbda30d5e54042
Brandnbloom <shreya.anaokar811@gmail.com>
2025-06-29 21:54:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ import streamlit.components.v1 as components
 components.html(
     """
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-0GBTQZDD53"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
