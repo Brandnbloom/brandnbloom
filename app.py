ee91b6e51655d9a3592a8a0544ee3dcaccc9b49c
Brandnbloom <shreya.anaokar811@gmail.com>
2025-06-29 15:41:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,8 @@ s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 </script>
-""", unsafe_allow_html=True)
+""", height=0, # Keeps layout clean
+)
 
 # ✅ Footer
 st.markdown(
