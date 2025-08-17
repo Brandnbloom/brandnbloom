8241202f05cfcf7c4d9371970f4318aa3ba7fdba
Brandnbloom <shreya.anaokar811@gmail.com>
2025-08-17 20:26:57 +0530

    Delete bloominsight_package directory

--- bloominsight_package/create_sample_db.py
+++
@@ -1,4 +0,0 @@
-from db import Database
-db = Database("data/sample_bloom.db")
-# use the API or db.insert_profile_snapshot to add sample snapshots
-print("Created sample DB at data/sample_bloom.db")
