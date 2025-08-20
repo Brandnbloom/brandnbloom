06d892a1ed89e6c94a3a953696b58761dce885af
Brandnbloom <shreya.anaokar811@gmail.com>
2025-08-20 14:33:23 +0530

    Update security.py

--- auth/security.py
+++ auth/security.py
@@ -1,8 +1,11 @@
 import bcrypt
 
+# When creating a new user (signup)
 def hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
-    return bcrypt.hashpw(password.encode('utf-8'), salt).decode('utf-8')
+    hashed = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return hashed.decode("utf-8")
 
+# When checking login (verify password)
 def verify_password(password: str, hashed: str) -> bool:
-    return bcrypt.checkpw(password.encode('utf-8'), hashed.encode('utf-8'))
+    return bcrypt.checkpw(password.encode("utf-8"), hashed.encode("utf-8"))
