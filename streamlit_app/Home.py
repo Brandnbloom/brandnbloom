becb2b92df3390b86ab6a3bd22ba5b55f90d8787
Brandnbloom <shreya.anaokar811@gmail.com>
2025-09-19 23:21:54 +0530

    Update Home.py

--- streamlit_app/Home.py
+++ streamlit_app/Home.py
@@ -1,8 +1,11 @@
 import streamlit as st
 from streamlit_app import Register, Login, Dashboard, SEOTools
 
-menu = ["Home", "Register", "Login", "Dashboard", "SEO Tools"]
+menu = ["Home", "Register", "Login", "Dashboard", "SEO Tools", "Social Tools"]
 choice = st.sidebar.selectbox("Menu", menu)
+
+if choice == "Social Tools":
+    SocialTools
 
 if choice == "Home":
     st.subheader("Welcome to Brand n Bloom SaaS")
