26f65ba8a2d9b5c683df4f8b71f3fd68d189622f
Brandnbloom <shreya.anaokar811@gmail.com>
2025-09-19 06:43:28 +0530

    Update __init__.py

---
+++ db/__init__.py
@@ -0,0 +1,15 @@
+# db.py
+from sqlmodel import SQLModel, create_engine, Session
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", os.getenv("DATABASE_URL", "sqlite:///./bnb.db"))
+engine = create_engine(DATABASE_URL, echo=False)
+
+def init_db():
+    from models import *   # create all models
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    return Session(engine)
