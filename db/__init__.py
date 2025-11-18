9b81c010d1c7b3c906fd378b52ca342b7f1cf5cc
Brandnbloom <shreya.anaokar811@gmail.com>
2025-11-18 10:07:05 +0530

    Update __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -1,10 +1,18 @@
 # db/__init__.py
 
-from .db import init_db, get_session
+from .database import engine, Base, get_db
 from .models import *
+
+# Create tables when imported
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
 
 __all__ = [
     "init_db",
-    "get_session",
+    "engine",
+    "Base",
+    "get_db",
     *[name for name in globals() if not name.startswith("_")]
 ]
+
