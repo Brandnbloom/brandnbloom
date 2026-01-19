d8e6d4b8ff3e3770bd2099cedd4c663722bb92bb
Brandnbloom <shreya.anaokar811@gmail.com>
2026-01-19 14:45:46 +0530

    Format import statements in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
-import streamlit as st from utils.ui import inject_css, dark_mode_toggle, card
+import streamlit as st 
+from utils.ui import inject_css, 
+dark_mode_toggle, card
 st.set_page_config( page_title="Brand n Bloom", layout="wide", initial_sidebar_state="collapsed" )
 st.markdown("""
 """, unsafe_allow_html=True)
