3e3e15914bef0e9d54566e93087f881f1ab3d754
Brandnbloom <shreya.anaokar811@gmail.com>
2026-01-19 19:50:14 +0530

    Add mock function to fetch consumer behavior data

---
+++ ai_tools/consumer_behavior_mock.py
@@ -0,0 +1,9 @@
+# ai_tools/consumer_behavior_mock.py
+
+def fetch_behavior_data():
+    return {
+        "engagement_rate": 0.062,
+        "repeat_customer_ratio": 0.48,
+        "price_sensitivity": 0.35,
+        "content_save_rate": 0.18
+    }
