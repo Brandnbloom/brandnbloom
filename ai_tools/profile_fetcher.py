93430f25a9b0a24f5b4488eefb09df413ee9989e
Brandnbloom <shreya.anaokar811@gmail.com>
2026-01-23 23:50:13 +0530

    Update profile_fetcher.py

--- ai_tools/profile_fetcher.py
+++ ai_tools/profile_fetcher.py
@@ -1,5 +1,21 @@
 import os
 import requests
+import pandas as pd
+
+sample_data = pd.DataFrame({
+    "customer_id": [1,2,3],
+    "recency": [10, 20, 5],
+    "frequency": [3, 1, 5],
+    "monetary": [200, 150, 500],
+    "churn": [0, 1, 0]
+})
+
+st.download_button(
+    "📥 Download Sample Data",
+    data=sample_data.to_csv(index=False),
+    file_name="sample_data.csv",
+    mime="text/csv"
+)
 
 class InstagramProfileFetcher:
     def __init__(self):
