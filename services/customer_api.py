6dd0a79eaa2640d3e7ab43d136aec8f66aeefd37
Brandnbloom <shreya.anaokar811@gmail.com>
2026-01-28 21:35:11 +0530

    Update customer_api.py

--- services/customer_api.py
+++ services/customer_api.py
@@ -6,9 +6,7 @@ import razorpay
 from google.analytics.data_v1beta import BetaAnalyticsDataClient
 from google.analytics.data_v1beta.types import RunReportRequest
 import json
-from services.customer_api import get_customer_data
 
-df = get_customer_data()
 
 # Load environment variables
 GA4_PROPERTY_ID = os.getenv("GA4_PROPERTY_ID")
