5d003a409234259ae0f71cd1d56d5217d7796711
Brandnbloom <shreya.anaokar811@gmail.com>
2026-02-16 12:29:16 +0530

    Delete scheduler directory

--- scheduler/scheduler.py
+++
@@ -1,7 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-
-def start_scheduler(job_func, minutes: int = 60):
-    sched = BackgroundScheduler()
-    sched.add_job(job_func, 'interval', minutes=minutes, id='weekly_report_job', replace_existing=True)
-    sched.start()
-    return sched
